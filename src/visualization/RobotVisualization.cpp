f9bdf3c81c5861b546c235950db3d448100046ab
Vojtěch Venzara <61122158+Vojtaven@users.noreply.github.com>
2025-11-03 14:18:49 +0100

    SFML visualization start

--- src/visualization/RobotVisualization.cpp
+++ src/visualization/RobotVisualization.cpp
@@ -1,1 +1,10 @@
 #include "RobotVisualization.hpp"
+
+
+void Visualization::DrawRobotFromConfig(sf::RenderTarget& target, const RoboConfig& config) {
+	sf::CircleShape circle;
+	circle.setPosition(sf::Vector2f(0, 0));
+	circle.setRadius(20);
+	target.draw(circle);
+}
+
--- src/visualization/RobotVisualization.hpp
+++ src/visualization/RobotVisualization.hpp
@@ -1,6 +1,12 @@
 #ifndef ROBOT_VISUALIZATION_HPP
 #define ROBOT_VISUALIZATION_HPP
 
+#include <SFML/Graphics.hpp>
+#include <RoboConfig.hpp>
 
+static class Visualization {
+public:
+	static void DrawRobotFromConfig(sf::RenderTarget& target, const RoboConfig& config);
+};
 
 #endif //ROBOT_VISUALIZATION_HPP
